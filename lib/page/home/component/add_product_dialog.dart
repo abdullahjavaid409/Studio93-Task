dabc84b6da5bb9f4d8e67719f7469c50f25bf8cb
Abdullah Javaid <abdullah.javaid0331@gmail.com>
2023-09-25 15:55:22 +0500

    update the coide

--- lib/page/home/component/add_product_dialog.dart
+++ lib/page/home/component/add_product_dialog.dart
@@ -10,6 +10,7 @@ class AddProductDialog extends StatefulWidget {
 class _AddProductDialogState extends State<AddProductDialog> {
   final productNameController = TextEditingController();
   final priceController = TextEditingController();
+  final formValidateKey = GlobalKey<FormState>();
 
   @override
   void dispose() {
@@ -20,37 +21,63 @@ class _AddProductDialogState extends State<AddProductDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      child: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            'Add Product'.toText(
-                fontSize: 20.sp,
-                fontWeight: FontWeight.bold,
-                textAlign: TextAlign.center),
-            const VerticalSpacing(of: 16.0),
-            TextField(
-              controller: productNameController,
-              decoration: const InputDecoration(labelText: 'Product Name'),
-            ),
-            const VerticalSpacing(of: 16.0),
-            TextField(
-              controller: priceController,
-              decoration: const InputDecoration(labelText: 'Price'),
-              keyboardType: TextInputType.number,
-            ),
-            const VerticalSpacing(of: 16.0),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColor.lightBlackColor),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Add Product'),
-            ),
-          ],
+    return Form(
+      key: formValidateKey,
+      child: Dialog(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              'Add Product'.toText(
+                  fontSize: 20.sp,
+                  fontWeight: FontWeight.bold,
+                  textAlign: TextAlign.center),
+              const VerticalSpacing(of: 16.0),
+              TextFormField(
+                decoration: const InputDecoration(
+                    labelText: 'Product Name',
+                    labelStyle: TextStyle(color: AppColor.lightBlackColor),
+                    border: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColor.lightBlackColor)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColor.lightBlackColor)),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColor.lightBlackColor))),
+                controller: productNameController,
+              ),
+              const VerticalSpacing(of: 16.0),
+              TextFormField(
+                controller: priceController,
+                decoration: const InputDecoration(
+                    labelText: 'Price',
+                    labelStyle: TextStyle(color: AppColor.lightBlackColor),
+                    border: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColor.lightBlackColor)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColor.lightBlackColor)),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColor.lightBlackColor))),
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+              ),
+              const VerticalSpacing(of: 16.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColor.lightBlackColor),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Add Product'),
+              ),
+            ],
+          ),
         ),
       ),
     );
